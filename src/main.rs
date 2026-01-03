2a2aa2b49868e00c79c7654d79b52addbe460c49
rikimaru takahashi <rikimarukun93@gmail.com>
2026-01-03 21:15:46 +0900

    fix

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,22 @@
 use std::fs::File;
-use std::io::Read;
+use std::io::{Read, Seek, SeekFrom};
 
 fn main() {
-    let mut file = File::open("/Users/takahashirikimaru/develop/rust/photapp/A40A8793.CR2")
-        .expect("ファイルが開けません");
+    let mut file = File::open("A40A8793.CR2").expect("ファイルが開けません");
     let mut header = [0u8; 8];
     file.read_exact(&mut header).unwrap();
     println!("{:x?}", header);
+
+    let le = match &header[0..2] {
+        b"II" => true,
+        b"MM" => false,
+        _ => panic!("不明なエンディアン"),
+    };
+    let ifd_offset = if le {
+        u32::from_le_bytes([header[4], header[5], header[6], header[7]]) as usize
+    } else {
+        u32::from_be_bytes([header[4], header[5], header[6], header[7]]) as usize
+    };
+    println!("IFDオフセット: {}", ifd_offset);
+    file.seek(SeekFrom::Start(ifd_offset as u64)).unwrap();
 }
